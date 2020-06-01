5ca0228b8a7337a986cea8d6ab0d76a37e89f22b
Kiko Bela <kiko_bela_mkd@hotmail.com>
2020-06-01 09:53:58 +0200

    ver_16

--- Data/ITShopDbContext.cs
+++ Data/ITShopDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 
 namespace Data
 {
@@ -17,5 +18,51 @@ namespace Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Membership> Memberships { get; set; }
         public DbSet<OrderDetail> OrderDetail { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(new List<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Type = "Mobile"
+                },
+                new Category
+                {
+                    Id = 2,
+                    Type = "TV"
+                },
+                new Category
+                {
+                    Id = 3,
+                    Type = "Audio"
+                }
+            });
+            modelBuilder.Entity<Membership>().HasData(new List<Membership>
+            {
+                new Membership
+                {
+                    Id = 1,
+                    Type = "Bronze",
+                    Discount = 0.05,
+                    Price = 25
+                },
+                new Membership
+                {
+                    Id = 2,
+                    Type = "Silver",
+                    Discount = 0.1,
+                    Price = 35
+                },
+                new Membership
+                {
+                    Id = 3,
+                    Type = "Gold",
+                    Discount = 0.15,
+                    Price = 45
+                }
+            });
+        }
     }
 }
---
+++ Data/Migrations/20200601074848_Seeding.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class Seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Mobile" },
+                    { 2, "TV" },
+                    { 3, "Audio" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Memberships",
+                columns: new[] { "Id", "Discount", "Price", "Type" },
+                values: new object[,]
+                {
+                    { 1, 0.050000000000000003, 25.0, "Bronze" },
+                    { 2, 0.10000000000000001, 35.0, "Silver" },
+                    { 3, 0.14999999999999999, 45.0, "Gold" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Memberships",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
